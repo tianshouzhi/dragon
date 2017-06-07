a8dc2ea989fa403eece66a40049a5e2989f563c4
tianshouzhi <tianshouzhi@meituan.com>
2017-06-07 13:20:49 +0800

    remove dragon-dynaminc-datasource module

--- dragon-dynamic-datasource/src/main/java/com/tianshouzhi/dragon/dynamic/config/DragonDynamicConfigSource.java
+++
@@ -1,9 +0,0 @@
-package com.tianshouzhi.dragon.dynamic.config;
-
-/**
- * Created by TIANSHOUZHI336 on 2017/2/19.
- */
-public interface DragonDynamicConfigSource {
-    public String getConfig(String remoteKey);
-    public void addListener();
-}
